ece00c90ea9792a1247a0a5d22d5de3efb161601
kirillstacenko <131430825+kirillstacenko@users.noreply.github.com>
2023-06-19 05:35:44 +0600

    Update server.js

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ import { createProxyMiddleware } from "http-proxy-middleware"
 import path from 'path'
 import {fileURLToPath} from 'url';
 dotenv.config();
-/*
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-*/
+
 connectDB();
 
 const app = express();
@@ -22,24 +22,24 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'));
-//app.use(express.static(path.join(__dirname, './client/build')))
+app.use(express.static(path.join(__dirname, './client/build')))
 
 
 //routers
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/category', categoryRoutes);
 app.use("/api/v1/product", productRoutes);
-
+/*
 app.get('/', (req,res) => {
     res.send("<h1>СЕРВЕР ЗАПУЩЕН</h1>")
 })
+*/
 
 
-/*
 app.use('*',function (req,res){
     res.sendFile(path.join(__dirname, './client/dist/index.html'));
 })
-*/
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, ()=> {
